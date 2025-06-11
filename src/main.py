cb5e87516cc6845ca9b5991a7c299c1101810663
sacridini <eduardolacerdageo@gmail.com>
2025-06-11 13:17:24 +0200

    updated code structure

--- src/main.py
+++ src/main.py
@@ -7,20 +7,6 @@ from scipy.signal import savgol_filter
 # a0 = 21
 # c8 = 108
 
-# Cria um novo arquivo MIDI com 1 trilha
-midi = MIDIFile(1)  # O número é a quantidade de trilhas
-
-# Adiciona informações básicas
-track = 0
-time = 0
-midi.addTrackName(track, time, "Minha Música")
-midi.addTempo(track, time, 120)  # 120 BPM
-
-# Adiciona notas
-channel = 0
-volume = 100
-duration = 1  # Em beats
-
 def extract_ndvi_from_csv(csv_path):
     df = pd.read_csv(csv_path)
     ndvi_values = df['NDVI'].to_numpy()
@@ -33,43 +19,67 @@ def extract_ndvi_from_csv_savgol(csv_path):
     return ndvi_savgol_result
 
 def ndvi_to_major_chord(ndvi_value):
-    # Normaliza o NDVI de [-1, 1] para [0, 1]
+    # Normalize the NDVI from [-1, 1] to [0, 1]
     normalized = (ndvi_value + 1) / 2
     root_index = np.round(normalized * (108 - 21) + 21).astype(int)
     chord_size = random.choice([3, 4])
     if chord_size == 3:
-        # Intervalos de tríade maior: fundamental, terça maior, quinta justa
+        # Intervals for a major chord
         major_chord = np.array([root_index, root_index + 4, root_index + 7])
     else:
-        # Intervalos de tétrade maior: fundamental, terça maior, quinta justa, sétima maior
+        # Intervals for a major seventh chord
         major_chord = np.array([root_index, root_index + 4, root_index + 7, root_index + 11])
-    # Verifica se algum valor do acorde é maior ou igual a 88 (maior que a capacidade de um piano padrão)
+    # Verify if any value in the chord is greater than or equal to 88 (greater than the capacity of a standard piano)
     if np.any(major_chord >= 88):
-        # Encontrar o valor mínimo no array
+        # Find the minimum value in the array
         valor_minimo = np.min(major_chord)
-        # Criar novo array com todos valores iguais ao mínimo
+        # Create a new array with all values equal to the minimum
         major_chord = np.full_like(major_chord, valor_minimo)
         return list(major_chord)
     else:
         return list(major_chord)
+    
+def create_midi(midi_name = "Minha Música", number_of_tracks = 1, tempo = 120, output_file_path = "output.mid"):
 
-if __name__ == "__main__":
-    # Extraindo valores NDVI do CSV sem Savgol
-    ndvi_values = extract_ndvi_from_csv("/Users/eduardolacerda/code/ndvi2midi/data/NDVI_Tijuca_v2.csv")
+    # Creates a new MIDI file with X number of track
+    midi = MIDIFile(number_of_tracks)
+
+    track = 0
+    time = 0
+    midi.addTrackName(track, time, midi_name)
+    midi.addTempo(track, time, tempo)
+
+    # Add note parameters
+    channel = 0
+    volume = 100
+    duration = 1  # Em beats
     
     for ndvi in ndvi_values:
         chord = ndvi_to_major_chord(ndvi)
         print(f"NDVI {ndvi} -> Chord MIDI notes: {chord}")
-        # Adiciona o acorde ao MIDI
-        for note in chord:
-            midi.addNote(track, channel, note, time, duration, volume)
-        time += duration
-    # Adiciona acordes baseados nos valores NDVI
+        
+    # Add chords to the MIDI file
+    for note in chord:
+        midi.addNote(track, channel, note, time, duration, volume)
+    time += duration
+    
+    # Add chords based on NDVI values
     for i, ndvi_value in enumerate(ndvi_values):
         chord = ndvi_to_major_chord(ndvi_value)
-        for note in chord:
-            midi.addNote(track, channel, note, time + i, duration, volume)
+        
+    for note in chord:
+        midi.addNote(track, channel, note, time + i, duration, volume)
             
-    # Salva o arquivo MIDI atualizado
-    with open("/Users/eduardolacerda/code/ndvi2midi/midi_files/minha_musica_com_acordes_tijuca_savgol_v2.mid", "wb") as output_file:
+    # Save the MIDI file
+    with open(output_file_path, "wb") as output_file:
         midi.writeFile(output_file)
+
+if __name__ == "__main__":
+    # Extracting NDVI values from CSV with Savgol filter
+    ndvi_values = extract_ndvi_from_csv("/Users/eduardolacerda/code/ndvi2midi/data/NDVI_Tijuca_v2.csv")
+    # Creating MIDI file with the extracted NDVI values
+    create_midi(midi_name="Minha Música Tijuca", 
+                number_of_tracks=1,
+                tempo=120, 
+                output_file_path="/Users/eduardolacerda/code/ndvi2midi/midi_files/minha_musica_com_acordes_tijuca_savgol_v2.mid")
+
